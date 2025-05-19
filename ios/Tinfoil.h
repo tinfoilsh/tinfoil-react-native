e235c4077b16b58f4231074c61d849e4297eea3d
Jules Drean <drean@mit.edu>
2025-05-19 00:03:47 -0400

    chore: add an old and new architecture target for CI

--- ios/Tinfoil.h
+++ ios/Tinfoil.h
@@ -1,14 +1,12 @@
+#if RCT_NEW_ARCH_ENABLED
+#import <Tinfoil/Tinfoil.h>
+
+@interface Tinfoil : NSObject <NativeTinfoilSpec>
+@end
+#else // Old architecture
 #import <React/RCTEventEmitter.h>
 #import <React/RCTBridgeModule.h>
 
-#if RCT_NEW_ARCH_ENABLED
-#import <Tinfoil/Tinfoil.h>
-#endif
-
 @interface Tinfoil : RCTEventEmitter <RCTBridgeModule>
-@end
-
-#if RCT_NEW_ARCH_ENABLED
-@interface Tinfoil : NSObject <NativeTinfoilSpec>
 @end
 #endif
